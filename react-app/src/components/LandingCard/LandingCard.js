e47a816ffcaa551bfc19ea2d6decaf956218971c
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2024-02-14 18:00:42 -0800

    responsive changes for landing page

--- react-app/src/components/LandingCard/LandingCard.js
+++ react-app/src/components/LandingCard/LandingCard.js
@@ -6,7 +6,7 @@ const LandingCard = ({ header, description, image, buttonText}) => {
   return (
       <div className="splash-card-contents">
         <img className="splash-card-img" src={image} alt="card-img" />
-        <h2 className="splash-card-header">{header}</h2>
+        <h3 className="splash-card-header">{header}</h3>
         <p className="splash-card-desc">{description}</p>
         <button className="splash-card-btn">{buttonText}</button>
       </div>
