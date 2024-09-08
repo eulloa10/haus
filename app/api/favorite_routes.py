757d3d6a732d83f682eba8161401b50792a86b81
Edgar Ulloa <36951879+eulloa10@users.noreply.github.com>
2024-09-07 20:36:30 -0700

    Refactor favorite routes

--- app/api/favorite_routes.py
+++ app/api/favorite_routes.py
@@ -1,37 +1,43 @@
-from flask import Blueprint, jsonify, request, Response
+import json
+from flask import Blueprint, Response
 from flask_login import login_required, current_user
 from app.models import db, Favorite
-import json
 
 favorite_routes = Blueprint('favorites', __name__)
 me_favorite_routes = Blueprint('me_favorites', __name__)
+
 
 @favorite_routes.route('/')
 def get_all_favorites():
     favorite_listings = Favorite.query.all()
     return {'favorite_listings': [favorite.to_dict() for favorite in favorite_listings]}
 
+
 @me_favorite_routes.route('/favorites')
 @login_required
 def get_all_user_favorites():
-    favorite_listings = Favorite.query.filter(Favorite.user_id == current_user.id)
+    favorite_listings = Favorite.query.filter(
+        Favorite.user_id == current_user.id)
     return {'user_favorite_listings': [favorite.to_dict() for favorite in favorite_listings]}
+
 
 @me_favorite_routes.route('/favorites/<int:favorite_id>')
 @login_required
 def get_a_user_favorite(favorite_id):
-  favorite_listing = Favorite.query.filter(Favorite.id == favorite_id).filter(Favorite.user_id == current_user.id).all()
+    favorite_listing = Favorite.query.filter(Favorite.id == favorite_id).filter(
+        Favorite.user_id == current_user.id).all()
 
-  if favorite_listing:
-    return {'favorite_listing': [favorite.to_dict() for favorite in favorite_listing]}
-  else:
-    return Response(json.dumps({'Error': 'Record not found'}), status=404)
+    if favorite_listing:
+        return {'favorite_listing': [favorite.to_dict() for favorite in favorite_listing]}
+    else:
+        return Response(json.dumps({'Error': 'Record not found'}), status=404)
+
 
 @me_favorite_routes.route('/favorites/<int:favorite_id>', methods=['DELETE'])
 @login_required
 def delete_favorite(favorite_id):
-  favorite = Favorite.query.get(favorite_id)
-  if favorite.user_id == current_user.id:
-    db.session.delete(favorite)
-    db.session.commit()
-    return {'Message': 'Favorite was successfully deleted'}
+    favorite = Favorite.query.get(favorite_id)
+    if favorite.user_id == current_user.id:
+        db.session.delete(favorite)
+        db.session.commit()
+        return {'Message': 'Favorite was successfully deleted'}
