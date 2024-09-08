ef56f30adeec5c52649188efce5dd595620cbd18
Edgar Ulloa <36951879+eulloa10@users.noreply.github.com>
2024-09-07 20:25:46 -0700

    Refactor image routes

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, jsonify, request, Response
+from flask import Blueprint, Response
 from flask_login import login_required, current_user
-from app.models import Image, Listing, db
-from app.forms import ImageForm
+from app.models import Image, db
 import json
 
 
@@ -13,13 +12,11 @@ def get_all_images():
     images = Image.query.all()
     return {'Images': [image.to_dict() for image in images]}
 
-
 @me_image_routes.route('/images')
 @login_required
 def get_all_user_images():
     images = Image.query.filter(Image.user_id == current_user.id)
     return {'user_images': [image.to_dict() for image in images]}
-
 
 @image_routes.route('/<int:image_id>')
 @login_required
@@ -29,7 +26,6 @@ def get_image(image_id):
     return {'Image': [image.to_dict() for image in single_image]}
   else:
     return Response(json.dumps({'Error': 'Image not found'}), status=404)
-
 
 @image_routes.route('/<int:image_id>', methods=['DELETE'])
 @login_required
