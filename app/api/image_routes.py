2b97a547bbd31665ce7eaf32076b06f205a98fb8
Edgar Ulloa <36951879+eulloa10@users.noreply.github.com>
2024-09-07 20:39:59 -0700

    Refactor image routes

--- app/api/image_routes.py
+++ app/api/image_routes.py
@@ -1,7 +1,8 @@
+import json
+
 from flask import Blueprint, Response
 from flask_login import login_required, current_user
 from app.models import Image, db
-import json
 
 
 image_routes = Blueprint('images', __name__)
@@ -12,26 +13,28 @@ def get_all_images():
     images = Image.query.all()
     return {'Images': [image.to_dict() for image in images]}
 
+
 @me_image_routes.route('/images')
 @login_required
 def get_all_user_images():
     images = Image.query.filter(Image.user_id == current_user.id)
     return {'user_images': [image.to_dict() for image in images]}
 
+
 @image_routes.route('/<int:image_id>')
 @login_required
 def get_image(image_id):
-  single_image = Image.query.get(image_id)
-  if single_image:
-    return {'Image': [image.to_dict() for image in single_image]}
-  else:
+    single_image = Image.query.get(image_id)
+    if single_image:
+        return {'Image': [image.to_dict() for image in single_image]}
     return Response(json.dumps({'Error': 'Image not found'}), status=404)
+
 
 @image_routes.route('/<int:image_id>', methods=['DELETE'])
 @login_required
 def delete_image(image_id):
-  image = Image.query.get(image_id)
-  if image.user_id == current_user.id:
-    db.session.delete(image)
-    db.session.commit()
+    image = Image.query.get(image_id)
+    if image.user_id == current_user.id:
+        db.session.delete(image)
+        db.session.commit()
     return {'Message': 'Image was successfully deleted'}
